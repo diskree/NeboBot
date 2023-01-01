c880a0594de66863e6634191c6f755fb28b20d55
diskree <diskriminant.was.taken@gmail.com>
2023-01-01 09:32:31 +0400

    Full automatic work + notifications with claim info

--- app/src/main/java/com/diskree/nebobot/MainActivity.kt
+++ app/src/main/java/com/diskree/nebobot/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.diskree.nebobot
 
+import android.Manifest.permission.POST_NOTIFICATIONS
 import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
-import android.os.Vibrator
 import android.view.Menu
 import android.view.MenuItem
 import android.webkit.JavascriptInterface
@@ -11,6 +16,9 @@ import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import com.diskree.nebobot.databinding.ActivityMainBinding
 import org.jsoup.Jsoup
@@ -19,7 +27,9 @@ class MainActivity : AppCompatActivity() {
 
     private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     private val handler: Handler by lazy { Handler(mainLooper) }
+    private var menu: Menu? = null
     private var currentActiveBot: Bot? = null
+    private var notificationCounter = 0
 
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -68,9 +78,17 @@ class MainActivity : AppCompatActivity() {
                 }
                 val victoryDoor = document.selectFirst("img.doorSel[src$=door_iron_big.png]")
                 if (victoryDoor != null) {
-                    vibrate(1000)
-                    binding.webViewLock.isVisible = false
-                    return
+                    val coinsCount = document.selectFirst("div.m5.cntr>span.amount>img[src$=mn_iron.png]+span")?.text()
+                    val xpCount = document.selectFirst("div.m5.cntr>span.amount>img[src$=star.png]+span")?.text()
+                    val snowflakesCount = document.selectFirst("div.m5.cntr>span.amount>img[src$=snowflake.png]+span")?.text()
+                    NotificationManagerCompat.from(this@MainActivity).notify(
+                            notificationCounter++,
+                            NotificationCompat.Builder(this@MainActivity, currentActiveBot!!.name)
+                                    .setSmallIcon(R.drawable.ic_done)
+                                    .setContentText("\uD83E\uDE99 + $coinsCount, ⭐️+ $xpCount, ❄️ + $snowflakesCount")
+                                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                                    .build()
+                    )
                 }
                 val buyKeyLink = document.selectFirst("a.tdu[href*=buyKeyLink]")
                 if (buyKeyLink != null) {
@@ -78,7 +96,7 @@ class MainActivity : AppCompatActivity() {
                     return
                 }
                 val lossDoor = document.selectFirst("img.doorSel[src$=door_wall.png]")
-                if (lossDoor != null) {
+                if (lossDoor != null || victoryDoor != null) {
                     loadGameUrl("doors")
                     return
                 }
@@ -94,6 +112,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_main, menu)
+        this.menu = menu
         return true
     }
 
@@ -104,12 +123,9 @@ class MainActivity : AppCompatActivity() {
                 item.setTitle(R.string.bot_auto_labyrinth_start)
                 setTitle(R.string.app_name)
                 binding.webViewLock.isVisible = false
+                binding.webView.stopLoading()
             } else {
-                currentActiveBot = Bot.AUTO_LABYRINTH
-                item.setTitle(R.string.bot_auto_labyrinth_stop)
-                setTitle(R.string.bot_auto_labyrinth_status)
-                binding.webViewLock.isVisible = true
-                binding.webView.reload()
+                startBot()
             }
             true
         }
@@ -135,9 +151,35 @@ class MainActivity : AppCompatActivity() {
         }, (300..500).random().toLong())
     }
 
-    private fun vibrate(millis: Long) {
-        val vibrator = getSystemService(VIBRATOR_SERVICE) as Vibrator
-        vibrator.vibrate(millis)
+    private fun startBot() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU && ContextCompat.checkSelfPermission(this@MainActivity, POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(arrayOf(POST_NOTIFICATIONS), POST_NOTIFICATION_PERMISSION_REQUEST_ID)
+            return
+        }
+
+        currentActiveBot = Bot.AUTO_LABYRINTH
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.app_name)
+            val descriptionText = getString(R.string.bot_auto_labyrinth_status)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(currentActiveBot!!.name, name, importance).apply {
+                description = descriptionText
+            }
+            val notificationManager: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+        menu?.findItem(R.id.auto_labyrinth)?.setTitle(R.string.bot_auto_labyrinth_stop)
+        setTitle(R.string.bot_auto_labyrinth_status)
+        binding.webViewLock.isVisible = true
+        binding.webView.reload()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        val isGranted = grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED
+        if (requestCode == POST_NOTIFICATION_PERMISSION_REQUEST_ID && isGranted) {
+            startBot()
+        }
     }
 
     enum class Bot {
@@ -146,5 +188,6 @@ class MainActivity : AppCompatActivity() {
 
     companion object {
         private const val GAME_URL_PREFIX = "http://nebomobi2.spaces-games.com/"
+        private const val POST_NOTIFICATION_PERMISSION_REQUEST_ID = 10
     }
 }
